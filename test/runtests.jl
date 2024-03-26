0a1471f9f1b4af75f7c20faa49f0437928e62737
Dennis Yatunin <dyatun@gmail.com>
2024-03-26 11:13:49 -0700

    Add unrolled functions and unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using SafeTestsets
 
 #! format: off
+@safetestset "Test and Analyze" begin @time include("test_and_analyze.jl") end
 @safetestset "Aqua" begin @time include("aqua.jl") end
 #! format: on
